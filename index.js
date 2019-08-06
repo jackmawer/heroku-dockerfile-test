256d971cb9cbba9c442ef647a4755e545b4250a2
Jack <jackmawer@users.noreply.github.com>
2019-08-06 18:05:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = process.env.port || 3000;
+
+const requestHandler = (req,res) => {
+	console.log("Req for URL: " + req.url);
+	res.writeHead(200);
+	res.end("<h1>Hello, World!</h1>");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (error) => {
+	if (error) throw error;
+	console.log(`App running on port ${port}`);
+})
