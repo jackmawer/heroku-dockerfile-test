48cd043c6ac142891c44108e16b56be6506012ab
Jack <jackmawer@users.noreply.github.com>
2019-08-06 18:19:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const requestHandler = (req,res) => {
 	console.log("Req for URL: " + req.url);
